fcc3d81fa9896fdac220d651967ce6e0184b9168
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2024-06-10 13:50:03 -0500

    replace exicution of query with ChatGPT code

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -8,7 +8,7 @@ const Readable = require('stream').Readable;
 const sftp = require('./sftp');
 const { log } = require('console');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const { RedshiftDataClient, ExecuteStatementCommand, DescribeStatementCommand } = require('@aws-sdk/client-redshift-data');
+const { RedshiftDataClient, ExecuteStatementCommand, GetStatementResultCommand} = require('@aws-sdk/client-redshift-data');
 
 const checkPartnerNetworkIn = partner => {
   if (!partner.network_in) {
@@ -252,36 +252,105 @@ module.exports.getOutgoingNotificationsForPartner = async event => {
     let result;
 
     if(triggerName) {
-      result = (`select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
-        from outgoing_notifications otn
-        inner join contacts c on otn.contact_id = c.id
-        left join integrations i on otn.integration_id = i.id
-        left join pixels p on otn.pixel_id IS NOT NULL AND otn.pixel_id = p.id and otn.partner_id = p.partner_id
-        left join partner_lists l on otn.partner_list_id IS NOT NULL AND otn.partner_list_id = l.id and otn.partner_id = l.partner_id
-        left join partner_triggers t on otn.integration_id = t.integration_id and l.trigger_id = t.id
-        where otn.partner_id = '${partner.id}'
-        AND t.name = '${triggerName}'
-        ${dateFiltersSql}
-        order by otn.date_sent desc
-        limit ${lmt} offset ${offst};`);
+      // result = (`select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
+      //   from outgoing_notifications otn
+      //   inner join contacts c on otn.contact_id = c.id
+      //   left join integrations i on otn.integration_id = i.id
+      //   left join pixels p on otn.pixel_id IS NOT NULL AND otn.pixel_id = p.id and otn.partner_id = p.partner_id
+      //   left join partner_lists l on otn.partner_list_id IS NOT NULL AND otn.partner_list_id = l.id and otn.partner_id = l.partner_id
+      //   left join partner_triggers t on otn.integration_id = t.integration_id and l.trigger_id = t.id
+      //   where otn.partner_id = '${partner.id}'
+      //   AND t.name = '${triggerName}'
+      //   ${dateFiltersSql}
+      //   order by otn.date_sent desc
+      //   limit ${lmt} offset ${offst};`);
 
-      // Create a RedshiftData client
-      const redshiftClient = new RedshiftDataClient({
-        region: process.env.MY_AWS_REGION,
-      });
+      // // Create a RedshiftData client
+      // const redshiftClient = new RedshiftDataClient({
+      //   region: process.env.MY_AWS_REGION,
+      // });
 
-      // Execute the UNLOAD command
-      const executeStatementResponse = await redshiftClient.send(
-        new ExecuteStatementCommand({
-          ClusterIdentifier: process.env.REDSHIFT_CLUSTER_IDENTIFIER,
-          Database: process.env.REDSHIFT_DATABASE,
-          DbUser: process.env.REDSHIFT_USER,
-          Sql: result,
-        })
-      );
+      // // Execute the UNLOAD command
+      // const executeStatementResponse = await redshiftClient.send(
+      //   new ExecuteStatementCommand({
+      //     ClusterIdentifier: process.env.REDSHIFT_CLUSTER_IDENTIFIER,
+      //     Database: process.env.REDSHIFT_DATABASE,
+      //     DbUser: process.env.REDSHIFT_USER,
+      //     Sql: result,
+      //   })
+      // );
 
-      console.log('EXECUTE STATMENT RESP: ', executeStatementResponse)
-      return main.responseWrapper(executeStatementResponse);
+      // console.log('EXECUTE STATMENT RESP: ', executeStatementResponse)
+      // return main.responseWrapper(executeStatementResponse);
+      const { RedshiftDataClient, ExecuteStatementCommand, GetStatementResultCommand } = require('@aws-sdk/client-redshift-data');
+
+      // Function to run the query and get the results
+      async function runQuery() {
+        // Your SQL query
+        const result = `select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
+              from outgoing_notifications otn
+              inner join contacts c on otn.contact_id = c.id
+              left join integrations i on otn.integration_id = i.id
+              left join pixels p on otn.pixel_id IS NOT NULL AND otn.pixel_id = p.id and otn.partner_id = p.partner_id
+              left join partner_lists l on otn.partner_list_id IS NOT NULL AND otn.partner_list_id = l.id and otn.partner_id = l.partner_id
+              left join partner_triggers t on otn.integration_id = t.integration_id and l.trigger_id = t.id
+              where otn.partner_id = '${partner.id}'
+              AND t.name = '${triggerName}'
+              ${dateFiltersSql}
+              order by otn.date_sent desc
+              limit ${lmt} offset ${offst};`;
+
+        // Create a RedshiftData client
+        const redshiftClient = new RedshiftDataClient({
+          region: process.env.MY_AWS_REGION,
+        });
+
+        // Execute the SQL statement
+        const executeStatementResponse = await redshiftClient.send(
+          new ExecuteStatementCommand({
+            ClusterIdentifier: process.env.REDSHIFT_CLUSTER_IDENTIFIER,
+            Database: process.env.REDSHIFT_DATABASE,
+            DbUser: process.env.REDSHIFT_USER,
+            Sql: result,
+          })
+        );
+
+        console.log('EXECUTE STATEMENT RESP: ', executeStatementResponse);
+
+        // Get the statement ID
+        const statementId = executeStatementResponse.Id;
+
+        // Fetch the results
+        let hasMoreResults = true;
+        let results = [];
+
+        while (hasMoreResults) {
+          const getStatementResultResponse = await redshiftClient.send(
+            new GetStatementResultCommand({
+              Id: statementId,
+            })
+          );
+
+          results = results.concat(getStatementResultResponse.Records);
+          hasMoreResults = getStatementResultResponse.NextToken !== undefined;
+
+          // If there are more results, use the NextToken to get the next set of results
+          if (hasMoreResults) {
+            getStatementResultResponse = await redshiftClient.send(
+              new GetStatementResultCommand({
+                Id: statementId,
+                NextToken: getStatementResultResponse.NextToken,
+              })
+            );
+          }
+        }
+
+        console.log('QUERY RESULTS: ', results);
+        return main.responseWrapper(results);
+      }
+
+      // Call the function
+      runQuery().catch(console.error);
 
     } else {
       result = await main.sql.query(
