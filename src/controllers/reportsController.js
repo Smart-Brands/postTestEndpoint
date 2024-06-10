f5746d0c62b7cfadfbe051793666b274a81eace2
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2024-06-10 14:30:09 -0500

    update Redshift query code

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -282,11 +282,11 @@ module.exports.getOutgoingNotificationsForPartner = async event => {
 
       // console.log('EXECUTE STATMENT RESP: ', executeStatementResponse)
       // return main.responseWrapper(executeStatementResponse);
-      const { RedshiftDataClient, ExecuteStatementCommand, GetStatementResultCommand } = require('@aws-sdk/client-redshift-data');
 
       // Function to run the query and get the results
       async function runQuery() {
         // Your SQL query
+        try {
         const result = `select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
               from outgoing_notifications otn
               inner join contacts c on otn.contact_id = c.id
@@ -347,11 +347,14 @@ module.exports.getOutgoingNotificationsForPartner = async event => {
 
         console.log('QUERY RESULTS: ', results);
         return main.responseWrapper(results);
+      } catch (err) {
+        console.log("ERROR: ", err)
+        return main.responseWrapper(results);
       }
-
-      // Call the function
-      runQuery().catch(console.error);
-
+    }
+      const queryResults = await runQuery();
+      console.log('Query Results: ', queryResults);
+      return queryResults;
     } else {
       result = await main.sql.query(
         `select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
