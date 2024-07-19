8150d7187be019c8fe8309c9e563389cab7aa516
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2024-07-19 15:37:05 -0500

    alias countQuery result

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -469,7 +469,7 @@ module.exports.postOutgoingNotificationsForPartner = async event => {
   queryParams.push(limit, offset);
 
   // Prepare the count query with the same where clause
-  let countQuery = `SELECT COUNT(*) FROM (select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
+  let countQuery = `SELECT COUNT(*) AS total FROM (select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
         from outgoing_notifications otn
         inner join contacts c on otn.contact_id = c.id
         left join integrations i on otn.integration_id = i.id
