f40fbc723a2d35fc176d4947ee67e9c3bf380e75
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2024-07-22 16:55:56 -0400

    updating getQueryResults to include totals

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -493,18 +493,6 @@ module.exports.postOutgoingNotificationsForPartner = async event => {
           LIMIT $2
           OFFSET $3`;
 
-    let countQuery = `SELECT COUNT(*) AS total FROM (select ${emlField}, i.name as integration_name, ifnull(p.uuid, 'Network') as uuid, ifnull(p.pixel_name, 'Network') as pixel_name, ifnull(p.description, 'Network') as pixel_description, ifnull(l.name, 'Pixel') as list_name, t.name as trigger_name, otn.*
-        from outgoing_notifications otn
-        inner join contacts c on otn.contact_id = c.id
-        left join integrations i on otn.integration_id = i.id
-        left join pixels p on otn.pixel_id IS NOT NULL AND otn.pixel_id = p.id and otn.partner_id = p.partner_id
-        left join partner_lists l on otn.partner_list_id IS NOT NULL AND otn.partner_list_id = l.id and otn.partner_id = l.partner_id
-        left join partner_triggers t on otn.integration_id = t.integration_id and l.trigger_id = t.id
-        WHERE 1 = 1
-        ${whereClause}
-        AND otn.partner_id = ?
-        AND otn.date_sent > DATE_SUB(NOW(), INTERVAL 30 DAY)) AS a`;
-
     queryParams.push(limit, offset);
 
     const client = new Client(REDSHIFT_CONFIG);
@@ -516,23 +504,7 @@ module.exports.postOutgoingNotificationsForPartner = async event => {
 
     try {
       const res = await client.query(query, queryParams);
-      const countParams = queryParams.slice(0, queryParams.length - 2);
-
-      // const totalResult = await main.sql.query(countQuery, countParams);
-      // const totalRecords = parseInt(totalResult[0].total, 10);
-
-      const totalResult = 100;
-      const totalRecords = 10;
-
-      const response = {
-        status: 'completed',
-        draw: draw,
-        recordsTotal: totalRecords,
-        recordsFiltered: totalRecords,
-        data: res.rows,
-      };
-
-      queries.set(newQueryId, response);
+      queries.set(newQueryId, { status: 'completed', data: res.rows });
     } catch (err) {
       queries.set(newQueryId, { status: 'error', error: err });
     } finally {
@@ -558,12 +530,22 @@ module.exports.postOutgoingNotificationsForPartner = async event => {
     }
 
     const queryStatus = queries.get(queryId);
+    const countParams = queryParams.slice(0, queryParams.length - 2);
+
+    const totalResult = await main.sql.query(countQuery, countParams);
+    const totalRecords = parseInt(totalResult[0].total, 10);
 
     if (queryStatus.status !== 'completed') {
       return main.responseWrapper({ status: 'error', error: 'Query not completed yet' });
     }
 
-    return main.responseWrapper({ status: 'completed', data: queryStatus.data });
+    return main.responseWrapper({
+      status: 'completed',
+      draw,
+      recordsFiltered: totalRecords,
+      recordsTotal: totalRecords,
+      data: queryStatus.data,
+    });
 };
 
 
